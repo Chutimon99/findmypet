32b0be00aa19cfc29231242cbf0fe56c1754fc70
chutimon.j <chutimonpang1@gmail.com>
2022-10-30 23:45:28 +0700

    complete

---
+++ app/src/main/java/com/example/findmypet/PetID.java
@@ -0,0 +1,14 @@
+package com.example.findmypet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetID extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_i_d);
+    }
+}
